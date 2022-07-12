6a03e791de00b422b244b34c222963752cbab034
Vladimir Murashka <Murashka.Vladimir@yandex.ru>
2022-07-12 21:39:28 +0300

    Create alertServise

--- ARGamePlanets/AlertServiсe.swift
+++ ARGamePlanets/AlertServiсe.swift
@@ -5,31 +5,62 @@
 //  Created by Swift Learning on 01.07.2022.
 //
 
-//import Foundation
-//import UIKit
-//
-//class MyAlert: UIViewController {
-//
-//
-//
-//
-//
-//
-//    func showAlert(with title: String, message: String) -> UIAlertController {
-//
-//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-//        alert.addAction(UIAlertAction(title: "Продолжить", style: UIAlertAction.Style.default, handler: nil))
-//        return alert
-//    }
-//
-//    func okAction() {
-//
-//    }
-//    
-//
-//
-//    func dismissAlert() {
-//
-//    }
-//
-//}
+import UIKit
+
+final class AlertManager {
+    
+    func showAlert(
+        fromViewController viewController: UIViewController,
+        title: String?,
+        message: String?,
+        firstButtonTitle: String?,
+        firstActionBlock: (() -> Void)?,
+        secondTitleButton: String?,
+        secondActionBlock: (() -> Void)?
+    ) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let firstAction = UIAlertAction(title: firstButtonTitle, style: .default, handler: { _ in
+            firstActionBlock?()
+        })
+        
+        let secondAction = UIAlertAction(title: secondTitleButton, style: .default) { _ in
+            secondActionBlock?()
+        }
+        
+        alertController.addAction(firstAction)
+        alertController.addAction(secondAction)
+        
+        viewController.present(alertController, animated: true)
+    }
+    
+    func showAlert(from viewController: UIViewController, alertModel: AlertModel) {
+        let alertController = UIAlertController(
+            title: alertModel.title,
+            message: alertModel.message,
+            preferredStyle: alertModel.preferredStyle
+        )
+        
+        for actionModel in alertModel.actions {
+            let action = UIAlertAction(title: actionModel.title, style: actionModel.style) { _ in
+                actionModel.actionBlock?()
+            }
+            alertController.addAction(action)
+        }
+        
+        viewController.present(alertController, animated: true)
+    }
+}
+
+struct AlertModel {
+    let title: String?
+    let message: String?
+    let preferredStyle: UIAlertController.Style
+    let actions: [ActionModel]
+}
+
+struct ActionModel {
+    let title: String?
+    let style: UIAlertAction.Style
+    let actionBlock: (() -> Void)?
+}
--- ARGamePlanets/ViewController.swift
+++ ARGamePlanets/ViewController.swift
@@ -9,100 +9,33 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+final class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet private weak var sceneView: ARSCNView!
+    @IBOutlet private weak var aimVert: UIView!
+    @IBOutlet private weak var aimHor: UIView!
+    @IBOutlet private weak var changeFlag: UIImageView!
+    
+    @IBOutlet private weak var earthButton: UIButton!
+    @IBOutlet private weak var jupiterButton: UIButton!
+    @IBOutlet private weak var marsButton: UIButton!
+    @IBOutlet private weak var mercuryButton: UIButton!
+    @IBOutlet private weak var moonButton: UIButton!
+    @IBOutlet private weak var neptuneButton: UIButton!
     
     private lazy var quitGameButton: UIButton = {
         let button = UIButton()
-        let imageQuitGameButton = UIImage.init(systemName: "arrowshape.turn.up.left.circle.fill")
+        let imageQuitGameButton = UIImage(systemName: "arrowshape.turn.up.left.circle.fill")
         button.setBackgroundImage(imageQuitGameButton, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(quitGameButtonPressed), for: .touchUpInside)
         return button
     }()
     
-    @objc func quitGameButtonPressed(_ sender: UIButton) {
-        //sceneView.session.pause()
-        let quitAlert = UIAlertController(title: "Вы хотите выйти?", message: "Прогресс не будет сохранен!", preferredStyle: .alert)
-        quitAlert.addAction(UIAlertAction(title: "Покинуть игру", style: .cancel, handler: { (Action) in
-            self.dismiss(animated: true, completion: nil)
-        }))
-        quitAlert.addAction(UIAlertAction(title: "Продолжить", style: .default, handler: { (Action) in
-            //let configuration = ARWorldTrackingConfiguration()
-            //self.sceneView.session.run(configuration)
-        }))
-        self.present(quitAlert, animated: true, completion: nil)
-    }
-    
-    @IBOutlet var sceneView: ARSCNView!
-    @IBOutlet weak var aimVert: UIView!
-    @IBOutlet weak var aimHor: UIView!
-    @IBOutlet weak var changeFlag: UIImageView!
-    
-    @IBOutlet weak var earthButton: UIButton!
-    @IBOutlet weak var jupiterButton: UIButton!
-    @IBOutlet weak var marsButton: UIButton!
-    @IBOutlet weak var mercuryButton: UIButton!
-    @IBOutlet weak var moonButton: UIButton!
-    @IBOutlet weak var neptuneButton: UIButton!
-    
-    
-    private let planets = [
-        UIImage(named: "earth.jpg")!,
-        UIImage(named: "jupiter.jpg")!,
-        UIImage(named: "mars.jpg")!,
-        UIImage(named: "mercury.jpg")!,
-        UIImage(named: "moon.jpg")!,
-        UIImage(named: "neptune.jpg")!,
-    ]
-    
-    private lazy var planetButtonsArray = [
-        earthButton,
-        jupiterButton,
-        marsButton,
-        mercuryButton,
-        moonButton,
-        neptuneButton
-    ]
-
-    
-    @IBAction func earthButtonPressed(_ sender: UIButton) {
-        method(sender)
-    }
-    
-    @IBAction func jupiterButtonPressed(_ sender: UIButton) {
-        method(sender)
-    }
-    
-    @IBAction func marsButtonPressed(_ sender: UIButton) {
-        method(sender)
-    }
-    
-    @IBAction func merceryButtonPressed(_ sender: UIButton) {
-        method(sender)
-    }
-    
-    @IBAction func moonButtonPressed(_ sender: UIButton) {
-        method(sender)
-    }
-    
-    @IBAction func neptuneButtonPressed(_ sender: UIButton) {
-        method(sender)
-    }
-    
     // Базовые функции
     override func viewDidLoad() {
         super.viewDidLoad()
-        sceneView.delegate
-        sceneView.scene.physicsWorld.contactDelegate = self
-        addPlanets()
         setupViewController()
-    }
-    
-    private func method(_ sender: UIButton) {
-        for planetButton in planetButtonsArray {
-            planetButton?.alpha = 0.5
-        }
-        sender.alpha = 1
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -116,7 +49,63 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    @IBAction func didTapPlanetButton(_ sender: UIButton) {
+        let planetButtons = [
+            earthButton,
+            jupiterButton,
+            marsButton,
+            mercuryButton,
+            moonButton,
+            neptuneButton
+        ]
+        
+        for planetButton in planetButtons {
+            planetButton?.alpha = 0.5
+        }
+        sender.alpha = 1
+    }
+    
+    @objc func quitGameButtonPressed(_ sender: UIButton) {
+        sceneView.session.pause()
+        
+//        let alertController = UIAlertController(
+//            title: "Вы хотите выйти?",
+//            message: "Прогресс не будет сохранен!",
+//            preferredStyle: .alert
+//        )
+//        let quitAction = UIAlertAction(title: "Покинуть игру", style: .cancel) { [weak self] _ in
+//            self?.dismiss(animated: true)
+//        }
+//        let continueAction = UIAlertAction(title: "Продолжить", style: .default) { [weak self] _ in
+//            let configuration = ARWorldTrackingConfiguration()
+//            self?.sceneView.session.run(configuration)
+//        }
+//
+//        alertController.addAction(quitAction)
+//        alertController.addAction(continueAction)
+//
+//        present(alertController, animated: true)
+        
+        AlertManager().showAlert(
+            fromViewController: self,
+            title: "Вы хотите выйти?",
+            message: "Прогресс не будет сохранен!",
+            firstButtonTitle: "Покинуть игру",
+            firstActionBlock: { [weak self] in
+                self?.dismiss(animated: true)
+            },
+            secondTitleButton: "Продолжить") { [weak self] in
+                let configuration = ARWorldTrackingConfiguration()
+                self?.sceneView.session.run(configuration)
+            }
+    }
+    
     private func setupViewController() {
+        sceneView.delegate = self
+        sceneView.scene.physicsWorld.contactDelegate = self
+        
+        addPlanets()
+        
         view.addSubview(quitGameButton)
         
         NSLayoutConstraint.activate([
@@ -127,29 +116,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
         ])
     }
+    
+    // Добавление планет каждого типа.
+    private func addPlanets() {
+        let planets = Planet.allCases
+        for i in planets {
+            addRandomPisitionPlanet(number: 5, planet: i)
+        }
+    }
+    
+    //Рандомное размещение планет (мешеней)
+    private func addRandomPisitionPlanet(number: Int, planet: Planet) {
+        for _ in 1...number {
+            let xPos = randomPosition(from: -1.5, to: 1.5)
+            let yPos = randomPosition(from: -1.5, to: 1.5)
+            let zPos = randomPosition(from: -4, to: 0)
+            
+            addPlanet(planet: planet, xPos: xPos, yPos: yPos, zPos: zPos)
+        }
+    }
+    
+    //Рандомайзер
+    private func randomPosition(from: Float, to: Float) -> Float {
+        return Float(arc4random()) / Float(UInt32.max) * (from - to) + to
+    }
+    
     //Создание планеты
-    private func addPlanet(planet: UIImage, xPos: Float, yPos: Float, zPos: Float) {
-        
+    private func addPlanet(planet: Planet, xPos: Float, yPos: Float, zPos: Float) {
         let sphere = SCNSphere(radius: 0.1)
         let planetNode = SCNNode()
         
         planetNode.geometry = sphere
         planetNode.position = SCNVector3(xPos, yPos, -1.5)
-        
-        switch planet{
-        case planets[0]:
-            planetNode.name = "green"
-        case planets[1]:
-            planetNode.name = "red"
-        case planets[2]:
-            planetNode.name = "purple"
-        case planets[3]:
-            planetNode.name = "gray"
-        case planets[4]:
-            planetNode.name = "orange"
-        default:
-            planetNode.name = "yellow"
-        }
+        planetNode.name = planet.rawValue
         
         let material = SCNMaterial()
         material.diffuse.contents = planet
@@ -164,30 +163,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.scene.rootNode.addChildNode(planetNode)
     }
     
-    //Рандомайзер
-    private func randomPosition(from: Float, to: Float) -> Float{
-        return Float(arc4random()) / Float(UInt32.max) * (from - to) + to
-    }
-    
-    //Рандомное размещение планет (мешеней)
-    func addRandomPisitionPlanet(number: Int, planet: UIImage) {
-        for _ in 1...number {
-            let xPos = randomPosition(from: -1.5, to: 1.5)
-            let yPos = randomPosition(from: -1.5, to: 1.5)
-            let zPos = randomPosition(from: -4, to: 0)
-            
-            addPlanet(planet: planet, xPos: xPos, yPos: yPos, zPos: zPos)
-        }
-    }
-    // Добавление планет каждого типа.
-    private func addPlanets() {
-        for i in planets {
-            addRandomPisitionPlanet(number: 5, planet: i)
-        }
+    // Огонь батарея, Огонь батальон)
+    private func fire(planet: Planet) {
+        let node = createShot(planet: planet)
+        let (direction, position) = getUserVector()
+        node.position = position
+        let nodeDirection = SCNVector3(direction.x*4, direction.y*4, direction.z*4)
+        node.physicsBody?.applyForce(nodeDirection, at: SCNVector3(0.1, 0, 0), asImpulse: true)
+        sceneView.scene.rootNode.addChildNode(node)
     }
     
     // Создание снаряда/пули
-    private func createShot(materialShot: UIImage) -> SCNNode {
+    private func createShot(planet: Planet) -> SCNNode {
         let shot = SCNSphere(radius: 0.03)
         let shotNode = SCNNode()
         shotNode.geometry = shot
@@ -195,43 +182,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         shotNode.physicsBody?.isAffectedByGravity = false
         
         let material = SCNMaterial()
-        material.diffuse.contents = materialShot
+        material.diffuse.contents = planet
         material.locksAmbientWithDiffuse = true
         shotNode.geometry?.materials = [material]
-        
-        switch materialShot{
-        case planets[0]:
-            shotNode.name = "green"
-        case planets[1]:
-            shotNode.name = "red"
-        case planets[2]:
-            shotNode.name = "purple"
-        case planets[3]:
-            shotNode.name = "gray"
-        case planets[4]:
-            shotNode.name = "orange"
-        default:
-            shotNode.name = "yellow"
-        }
-        
+        shotNode.name = planet.rawValue
         shotNode.physicsBody?.categoryBitMask = CollisionCategory.missleCategory.rawValue
         shotNode.physicsBody?.contactTestBitMask = CollisionCategory.targetCategory.rawValue
         
         return shotNode
     }
     
-    // Огонь
-    private func fire(materalShot: UIImage) {
-        let node = createShot(materialShot: materalShot)
-        let (direction, position) = getUserVector()
-        node.position = position
-        let nodeDirection = SCNVector3(direction.x*4, direction.y*4, direction.z*4)
-        node.physicsBody?.applyForce(nodeDirection, at: SCNVector3(0.1, 0, 0), asImpulse: true)
-        sceneView.scene.rootNode.addChildNode(node)
-    }
     // поиск позиции и вектора устройства в прострвнстве
-    private func getUserVector() -> (SCNVector3, SCNVector3){
-        if let frame = self.sceneView.session.currentFrame{
+    private func getUserVector() -> (SCNVector3, SCNVector3) {
+        if let frame = self.sceneView.session.currentFrame {
             let mat = SCNMatrix4(frame.camera.transform)
             let dir = SCNVector3(-1 * mat.m31, -1 * mat.m32, -1 * mat.m33)
             let pos = SCNVector3(mat.m41, mat.m42, mat.m43)
@@ -239,17 +202,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
         return (SCNVector3(0, 0, -1), SCNVector3(0, 0, -0.2))
     }
+    
     // запуск при косании к экрану
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        fire(materalShot: changeFlag.image!)
+        //fire(planet: changeFlag.image!)
     }
 }
 
 // обработка столкновений
 extension ViewController: SCNPhysicsContactDelegate{
     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
-        
-        if contact.nodeA.name == contact.nodeB.name{
+        if contact.nodeA.name == contact.nodeB.name {
             DispatchQueue.main.async {
                 AudioServicesPlayAlertSoundWithCompletion(SystemSoundID(kSystemSoundID_Vibrate)) { }
                 contact.nodeA.removeFromParentNode()
@@ -258,4 +221,3 @@ extension ViewController: SCNPhysicsContactDelegate{
         }
     }
 }
-
